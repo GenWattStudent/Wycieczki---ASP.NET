15404b0ed2fc5d8be03288d0e58a09877c3c0078
GenWatt <adrian.kosoglos@gmail.com>
2024-04-19 20:27:46 +0200

    change login and register layout, change logo, add automatic scroll for side bar add suport for small height screens

--- wwwroot/js/three/earthAnimation.js
+++ wwwroot/js/three/earthAnimation.js
@@ -2,36 +2,16 @@ import EarthScene from './earth.js'
 import { Waypoint } from '../Waypoint.js'
 import { Assets } from './Assets.js'
 
-const waypoint1 = new Waypoint(
-  51.5074,
-  0.1278,
-  new Date().toISOString(),
-  'London'
-)
+const waypoint1 = new Waypoint(51.5074, 0.1278, new Date().toISOString(), 'London')
 waypoint1.type = 'start'
 
-const waypoint2 = new Waypoint(
-  40.7128,
-  -74.006,
-  new Date().toISOString(),
-  'New York'
-)
+const waypoint2 = new Waypoint(40.7128, -74.006, new Date().toISOString(), 'New York')
 waypoint2.type = 'road'
 
-const waypoint3 = new Waypoint(
-  35.6895,
-  139.6917,
-  new Date().toISOString(),
-  'Tokyo'
-)
+const waypoint3 = new Waypoint(35.6895, 139.6917, new Date().toISOString(), 'Tokyo')
 waypoint3.type = 'marker'
 
-const waypoint4 = new Waypoint(
-  37.7749,
-  -122.4194,
-  new Date().toISOString(),
-  'San Francisco'
-)
+const waypoint4 = new Waypoint(37.7749, -122.4194, new Date().toISOString(), 'San Francisco')
 waypoint4.type = 'end'
 
 const waypoints = [waypoint1, waypoint2, waypoint3, waypoint4]
@@ -41,7 +21,13 @@ const assets = new Assets()
 assets.loadAssets().then(() => {
   const earthScence = new EarthScene(assets)
   earthScence.earth.setWaypoints(waypoints)
-
+  const main = $('main')
+  const mainWidth = main.width()
+  earthScence.earth.earthRadius = 5.5
   earthScence.earth.drawEarth()
+
+  earthScence.setWidth(mainWidth / 2)
+  earthScence.setHeight(mainWidth / 2)
+
   earthScence.animate()
 })
